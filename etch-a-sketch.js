421badc0ba23c9b0dc2fc7dc9f02fc19586b726d
Jason <katsuutan0408@gmail.com>
2023-11-26 21:53:14 -0800

    Add randomized RGB to each square

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,12 +5,10 @@ function createGrid(gridSize) {
         const rowContainer = document.createElement('div');
         containerDiv.appendChild(rowContainer);
     
-        // Places 16 squares in each row
         for (let j = 0; j < gridSize; j++) {
             createSquare(rowContainer, 500/gridSize);
         }
     }
-    /*setSquareSize(500/gridSize);*/
 }
 
 function createSquare(rowContainer, size) {
@@ -22,18 +20,25 @@ function createSquare(rowContainer, size) {
     // Hover effect by adding a new class to each square if mouseover
     squareDiv.addEventListener('mouseover', function(e) {
         e.target.classList.add('active');
+        // Sets a randomized background color for each square on a mouseover event.
+        squareDiv.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()}`;
     });
 
     rowContainer.appendChild(squareDiv);
 }
 
+// Returns a random number from 0 - 255.
+function randomRGB(){
+    return (Math.round(Math.random()*255));
+}
+
 function resetGrid() {
-    //While there is still a square, removes the square until there's none left.
+    // While there is still a square, removes the square until there's none left.
     while(containerDiv.firstChild){
         containerDiv.removeChild(container.firstChild);
     }
 
-    //Creates a newGrid based on the size given to the prompt (1-100).
+    // Creates a newGrid based on the size given to the prompt (1-100).
     let resetSize;
     do {
         resetSize = parseInt(prompt ('Enter a number to create a new grid (1-100).'));
